d2d6171512c25bda4f3f7e2bcd70fdc36b4717a2
greatyyx <bigyyx@gmail.com>
2019-01-20 15:53:39 -0800

    fix slug error caused by pelican upgrade

--- publishconf.py
+++ publishconf.py
@@ -15,7 +15,7 @@ SITEURL = 'http://blog.yyx.me'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS=['sitemap',]
