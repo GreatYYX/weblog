a258458555c8c1454d695308545df7bb7a68fd3e
greatyyx <bigyyx@gmail.com>
2019-01-29 11:44:01 -0800

    add image caption from image title

--- theme/MinimalXYZ/static/js/main.js
+++ theme/MinimalXYZ/static/js/main.js
@@ -7,6 +7,11 @@ $(document).ready(function(){
 		hljs.highlightBlock(block);
 	});
 
+	/* image caption */
+	$('img[title]').each(function(i, block){
+		$('<div class="image-caption">' + $(this).attr('title') + '</div>').insertAfter($(this));
+	});
+
 	/* external link */
 	var host = new RegExp('/' + window.location.host + '/');
 	$('article section#content a').each(function() {
